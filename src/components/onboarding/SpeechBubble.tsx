69ee28955ddddf3412e7d446723c483a86272941
daejlee <savif4991@gmail.com>
2026-08-15 11:51:31 +0900

    fix: 말풍선 좌우 패딩 30px로 조정

--- src/components/onboarding/SpeechBubble.tsx
+++ src/components/onboarding/SpeechBubble.tsx
@@ -13,7 +13,7 @@ export default function SpeechBubble({ speaker = "토키", message, className }:
   return (
     <div className={cn("relative", className)}>
       <Chip className="bg-point absolute top-0 left-5 -translate-y-1/2 text-white">{speaker}</Chip>
-      <div className="bg-gray-00 rounded-[15px] px-5 pt-7 pb-5">
+      <div className="bg-gray-00 rounded-[15px] px-[30px] pt-7 pb-5">
         <Typography
           variant="sb3"
           as="div"
