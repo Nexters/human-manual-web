e19bab55457f26197509adf45b83aa3a8035dbd5
daejlee <savif4991@gmail.com>
2026-08-22 01:34:35 +0900

    feat: 문구수정

--- src/components/result/keyFeatures/index.tsx
+++ src/components/result/keyFeatures/index.tsx
@@ -28,7 +28,7 @@ export default function KeyFeatures({
   return (
     <div className="flex flex-col gap-6 px-5 py-8">
       {/* ----- 상단 타이틀 섹션 ----- */}
-      <SectionTitle title="핵심 특징" subtitle="나를 기분 좋게 만드는 행동들이에요" />
+      <SectionTitle title="핵심 특징" subtitle="나는 이렇게 작동해요" />
 
       {/* ----- 장난감 이미지 섹션 ----- */}
       <div className="flex items-center justify-center">
